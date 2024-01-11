f238a67e5434a39ad59a74e000f57bc12915e777
H.Ahmadi <hassanahmadi9689@gmail.com>
2024-01-11 22:58:06 +0330

    deleted comment onuser purchase

--- UserPurchases.cs
+++ UserPurchases.cs
@@ -1,26 +1,5 @@
 namespace ConsoleApp84;
 
-//public class User
-//{
-    //public User(string username)
-    //{
-        //Username = username;
-      //  _UserPurchasesList = new ();
-    //}
-    
-    //public string Username { get; set; }
-    //private List<UserPurchases> _UserPurchasesList;
-
-    //public List<UserPurchases> GetPurchasesList()
-    //{
-      //  return _UserPurchasesList;
-    //}
-
-    //public void setpurchases(FastFood food)
-    //{
-    //    _UserPurchasesList.Add(new UserPurchases(this,food.Name,food.Price));
-  //  }
-//}
 public class UserPurchases
 {
     public UserPurchases(string user, FastFood food)
